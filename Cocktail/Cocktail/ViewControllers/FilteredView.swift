09adc90549e8ac807d95d7629e9ac0da1d845625
fimuxd <me@boyoung.dev>
2021-11-25 18:39:46 +0900

    FilterView 리팩토링

--- Cocktail/Cocktail/ViewControllers/FilteredView.swift
+++ Cocktail/Cocktail/ViewControllers/FilteredView.swift
@@ -17,24 +17,23 @@ class FilteredView: UIView {
     
     var cellIsChecked: [[Bool]] = []
     
-    var baseCondition: [Cocktail.Base] = []
-    var alcoholCondition: [Cocktail.Alcohol] = []
-    var drinkTypeCondition: [Cocktail.DrinkType] = []
-    var craftConditon: [Cocktail.Craft] = []
-    var glassCondition: [Cocktail.Glass] = []
-    var colorCondition: [Cocktail.Color] = []
-
-    let alcoholSection = Cocktail.Alcohol.allCases
-    let baseSection = Cocktail.Base.allCases
-    let drinkTypeSection = Cocktail.DrinkType.allCases
-    let craftSection = Cocktail.Craft.allCases
-    let glassSection = Cocktail.Glass.allCases
-    let colorSection = Cocktail.Color.allCases
+    typealias FilterData = (condition: [CocktailCondition], section: [CocktailCondition])
+    var conditionsOfCocktail = [FilterData]()
     
     let componentsOfCocktail: [[String]] = [Cocktail.Alcohol.allCases.map {$0.rawValue}, Cocktail.Base.allCases.map {$0.rawValue}, Cocktail.DrinkType.allCases.map {$0.rawValue}, Cocktail.Craft.allCases.map {$0.rawValue}, Cocktail.Glass.allCases.map {$0.rawValue}, Cocktail.Color.allCases.map {$0.rawValue} ]
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
+        self.conditionsOfCocktail = [
+            (condition: [Cocktail.Alcohol](), section: Cocktail.Alcohol.allCases),
+            (condition: [Cocktail.Base](), section: Cocktail.Base.allCases),
+            (condition: [Cocktail.DrinkType](), section: Cocktail.DrinkType.allCases),
+            (condition: [Cocktail.Craft](), section: Cocktail.Craft.allCases),
+            (condition: [Cocktail.Glass](), section: Cocktail.Glass.allCases),
+            (condition: [Cocktail.Color](), section: Cocktail.Color.allCases)
+        ]
+        
         self.cellIsChecked = componentsOfCocktail.map {
             $0.map { _ in false }
         }
@@ -179,97 +178,28 @@ extension FilteredView: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "filterCell", for: indexPath) as? FilterViewCell else { return UITableViewCell()}
-
+        
         cell.isChecked = cellIsChecked[indexPath.section][indexPath.row]
         cell.nameLabel.text = componentsOfCocktail[indexPath.section][indexPath.row].localized
-
+        
         return cell
     }
-  
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         guard let cell = tableView.cellForRow(at: indexPath) as? FilterViewCell else { return }
         
-//        if cellIsChecked[indexPath.section][indexPath.row] == true {
-//            cellIsChecked[indexPath.section][indexPath.row] = false
-//            cell.isChecked = cellIsChecked[indexPath.section][indexPath.row]
-//            guard let number = alcoholCondition.firstIndex(of: alcoholSection[indexPath.row]) else { return }
-//            conditionsOfCocktail[indexPath.section].remove(at: number)
-//        } else {
-//            cellIsChecked[indexPath.section][indexPath.row] = true
-//            print(cellIsChecked)
-//            conditionsOfCocktail[indexPath.section].append(alcoholSection[indexPath.row])
-//            cell.isChecked = cellIsChecked[indexPath.section][indexPath.row]
-//        }
-
-        switch indexPath.section {
-        case 0:
-            if cellIsChecked[0][indexPath.row] == true {
-                cellIsChecked[0][indexPath.row] = false
-                cell.isChecked = cellIsChecked[0][indexPath.row]
-                guard let number = alcoholCondition.firstIndex(of: alcoholSection[indexPath.row]) else { return }
-                alcoholCondition.remove(at: number)
-            } else {
-                cellIsChecked[0][indexPath.row] = true
-                alcoholCondition.append(alcoholSection[indexPath.row])
-                cell.isChecked = cellIsChecked[0][indexPath.row]
-            }
-        case 1:
-            if cellIsChecked[1][indexPath.row] == true {
-                cellIsChecked[1][indexPath.row] = false
-                cell.isChecked = cellIsChecked[1][indexPath.row]
-                guard let number = baseCondition.firstIndex(of: baseSection[indexPath.row]) else { return }
-                baseCondition.remove(at: number)
-            } else {
-                cellIsChecked[1][indexPath.row] = true
-                cell.isChecked = cellIsChecked[1][indexPath.row]
-                baseCondition.append(baseSection[indexPath.row])
-            }
-        case 2:
-            if cellIsChecked[2][indexPath.row] == true {
-                cellIsChecked[2][indexPath.row] = false
-                cell.isChecked = cellIsChecked[2][indexPath.row]
-                guard let number = drinkTypeCondition.firstIndex(of: drinkTypeSection[indexPath.row]) else { return }
-                drinkTypeCondition.remove(at: number)
-            } else {
-                cellIsChecked[2][indexPath.row] = true
-                cell.isChecked = cellIsChecked[2][indexPath.row]
-                drinkTypeCondition.append(drinkTypeSection[indexPath.row])
-            }
-        case 3:
-            if cellIsChecked[3][indexPath.row] == true {
-                cellIsChecked[3][indexPath.row] = false
-                cell.isChecked = cellIsChecked[3][indexPath.row]
-                guard let number = craftConditon.firstIndex(of: craftSection[indexPath.row]) else { return }
-                craftConditon.remove(at: number)
-            } else {
-                cellIsChecked[3][indexPath.row] = true
-                cell.isChecked = cellIsChecked[3][indexPath.row]
-                craftConditon.append(craftSection[indexPath.row])
-            }
-        case 4:
-            if cellIsChecked[4][indexPath.row] == true {
-                cellIsChecked[4][indexPath.row] = false
-                cell.isChecked = cellIsChecked[4][indexPath.row]
-                guard let number = glassCondition.firstIndex(of: glassSection[indexPath.row]) else { return }
-                glassCondition.remove(at: number)
-            } else {
-                cellIsChecked[4][indexPath.row] = true
-                glassCondition.append(glassSection[indexPath.row])
-                cell.isChecked = cellIsChecked[4][indexPath.row]
-            }
-        case 5:
-            if cellIsChecked[5][indexPath.row] == true {
-                cellIsChecked[5][indexPath.row] = false
-                cell.isChecked = cellIsChecked[5][indexPath.row]
-                guard let number = colorCondition.firstIndex(of: colorSection[indexPath.row]) else { return }
-                colorCondition.remove(at: number)
-            } else {
-                cellIsChecked[5][indexPath.row] = true
-                colorCondition.append(colorSection[indexPath.row])
-                cell.isChecked = cellIsChecked[5][indexPath.row]
-            }
-        default:
-            break
+        if cellIsChecked[indexPath.section][indexPath.row] == true {
+            cellIsChecked[indexPath.section][indexPath.row] = false
+            cell.isChecked = cellIsChecked[indexPath.section][indexPath.row]
+            let conditions = conditionsOfCocktail[indexPath.section].condition
+            let section = conditionsOfCocktail[indexPath.section].section
+            guard let number = conditions.firstIndex(where: { $0.rawValue == section[indexPath.row].rawValue}) else { return }
+            conditionsOfCocktail[indexPath.section].condition.remove(at: number)
+        } else {
+            cellIsChecked[indexPath.section][indexPath.row] = true
+            let section = conditionsOfCocktail[indexPath.section].section
+            conditionsOfCocktail[indexPath.section].condition.append(section[indexPath.row])
+            cell.isChecked = cellIsChecked[indexPath.section][indexPath.row]
         }
     }
 }
