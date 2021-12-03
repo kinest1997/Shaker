f94e5308631b7d510fb42facce16dd2837829147
Heesung <kinest1997@naver.com>
2021-12-03 14:58:41 +0900

    Update Cocktail/Cocktail/SceneDelegate.swift
    Co-authored-by: Bo-Young PARK <me@boyoung.dev>

--- Cocktail/Cocktail/SceneDelegate.swift
+++ Cocktail/Cocktail/SceneDelegate.swift
@@ -10,8 +10,6 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-    
-    
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
